430dd2ce4cfe61f7be3f7886553877ff6f8e2cbd
Etem Senel <etemsenel96@hotmail.de>
2022-11-07 09:46:08 +0300

    first commit

---
+++ components/LessonIntroduction.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const LessonIntroduction = () => {
+  return (
+    <View>
+      <Text>LessonIntroduction</Text>
+    </View>
+  );
+};
+
+export default LessonIntroduction;
